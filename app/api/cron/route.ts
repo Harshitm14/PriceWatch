a39420814a348c9f3997727b7f6eab4bbb77b563
Harshitm14 <harshitmania3@gmail.com>
2024-06-27 04:08:38 +0530

    fixed build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,6 +6,10 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { NextResponse } from "next/server";
 import { title } from "process";
 
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
 export async function GET(){
     try {
         connectToDB();
@@ -36,7 +40,7 @@ export async function GET(){
                     }
                 
                 const updatedProduct = await Product.findOneAndUpdate(
-                    {url: scrapedProduct.url},
+                    {url: product.url},
                     product,
                 );
 
